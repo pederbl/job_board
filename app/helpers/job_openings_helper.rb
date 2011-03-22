ebc184ccdd91074226ce2d04a7fa1d8849046d89
Peder Linder <pederbl@gmail.com>
2011-03-22 12:12:33 +0000

    title

--- app/helpers/job_openings_helper.rb
+++ app/helpers/job_openings_helper.rb
@@ -2,7 +2,14 @@ module JobOpeningsHelper
 
   def title
     if params[:action] == 'show'
-      "Jobb: #{@job_opening.t(:title)}"
+      arr = []
+      arr << @job_opening.t(:title) if @job_opening.title
+      at = []
+      at << @job_opening.employer.name if @job_opening.employer.name
+      at << @job_opening.location.region if @job_opening.location.try(:region)
+      at << @job_opening.location.country if @job_opening.location.try(:country)
+      arr << at.join(", ") if at.present?
+      "Jobb: #{arr.join(" @ ")}"
     else
       "Lediga jobb"
     end
